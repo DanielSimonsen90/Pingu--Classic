0f908f72f3a263bcf73b9b9639d3e7cad3c092d2
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-03-01 15:21:56 +0100

    restart

--- commands/4 DevOnly/login.js
+++
@@ -1,6 +0,0 @@
-const { PinguCommand } = require('PinguPackage');
-module.exports = new PinguCommand('login', 'DevOnly', `Relogs me`, null, async () => {
-    var shell = require('shelljs');
-    shell.exec('git pull');
-    process.exit(1);
-})
---
+++ commands/4 DevOnly/restart.js
@@ -0,0 +1,6 @@
+﻿const { PinguCommand } = require('PinguPackage');
+module.exports = new PinguCommand('restart', 'DevOnly', `Relogs me`, null, async ({ message }) => {
+    await message.react('👌');
+    require('shelljs').exec('git pull');
+    process.exit(1);
+})
