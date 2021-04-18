12a3a375f42a811667e70ae9eb16ab4cfa433844
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-04-18 15:10:38 +0200

    Notice message

--- commands/4 DevOnly/test.js
+++ commands/4 DevOnly/test.js
@@ -1,66 +1,44 @@
 ﻿//const { _ } = require('discord.js');
 const { PinguCommand, PinguLibrary, PinguUser, PinguGuild, TimeLeftObject } = require('PinguPackage');
+const ms = require('ms');
 
 module.exports = new PinguCommand('test', 'DevOnly', `Test command for Danho`, {
 
 },
     async ({ client, message, args, pAuthor, pGuildMember, pGuild, pGuildClient }) => {
-        const startTime = Date.now();
+        const pUsers = await PinguUser.GetPUsers();
+        const users = (await Promise.all(pUsers.map(pu => client.users.fetch(pu._id)))).filter(v => v);
 
-        const [Guilds, Users] = await Promise.all([
-            PinguGuild.GetPGuilds(),
-            PinguUser.GetPUsers()
-        ]);
-        const getUser = (id) => client.users.cache.get(id);
+        users.forEach(async user => {
+            const dm = await user.createDM();
+            const pUser = pUsers.find(pu => pu._id == user.id)
+            const serversShared = pUser.sharedServers.length;
 
-        await Promise.all(client.commands
-            .filter(cmd =>
-                cmd.name.startsWith('updatep') &&
-                cmd.name.endsWith('s')
-            ).map(cmd => cmd.execute({ client, message, args, pAuthor, pGuildMember, pGuild, pGuildClient })))
+            try {
+                const sent = await dm.send(
+                    `Hello!\n` +
+                    `\n` +
+                    `If you don't know me, we share ${(serversShared > 1 ? `${serversShared} servers` : "a server")} together and therefore I want you to know this...` +
+                    `I have recently been upgraded with an achievement system, and so I will most likely send you a few congratulation messages, for achieveing pre-defined achievements.\n` +
+                    `Please note that this might become annoying - especially in the "start" - so you are welcome to block me if it bothers you.\n` +
+                    `\n` +
+                    `Since I share ${(serversShared > 1 ? "more than one" : "a")} server with you, I have your Discord account as an entry in my database. ` +
+                    `You can see what save from you, by using my \`${client.DefaultPrefix}info user all\` command.\n` +
+                    `If you wish to be deleted from my database, you are welcome to react to this message - although I'm programmed to add you automatically again, if you use one of my features...\n` +
+                    `\n` +
+                    `Thank you for understanding, and have an amazing day ${(PinguLibrary.getEmote(client, "hypers", PinguLibrary.SavedServers.PinguSupport(client)))}`
+                );
+                await sent.react('🗑️');
+                sent.createReactionCollector((reaction, user) => !user.bot, { time: ms('24h') }).on('collect', (reaction, user) => {
+                    await PinguUser.DeletePUser(client, user, module.exports.name,
+                        `Successfully deleted ${user.tag} from my database, after reacting to the notice message in DMs.`,
+                        `Failed to delete ${user.tag} from my database, after reacting to the notice message in DMs.`
+                    );
+                    return dm.send("Your data has been deleted!");
+                })
 
-        for (var pGuild of Guilds) {
-            const guild = client.guilds.cache.get(pGuild._id);
-            if (!guild) continue;
+            } catch { return; }
 
-            const ownerData = { guild, user: getUser(guild.ownerID), guildMember: guild.owner };
+        })
 
-            //members
-            for (var [pgmID, pgm] of pGuild.members) {
-                const user = client.users.cache.get(pgmID);
-                const member = guild.member(user);
-                const data = { guild, user, guildMember: member }
-
-                await PinguLibrary.AchievementCheck(client, data, 'EVENT', 'guildMemberAdd', [member]);
-            }
-
-            //owner
-            await PinguLibrary.AchievementCheck(client, ownerData, 'EVENT', 'guildCreate', [guild]);
-        }
-
-        for (var pUser of Users) {
-            const user = getUser(pUser._id);
-            if (!user) continue;
-
-            if (pUser.marry.partner) await PinguLibrary.AchievementCheck(client, { user }, 'COMMAND', 'marry', [{}]);
-            if (pUser.daily.streak) await PinguLibrary.AchievementCheck(client, { user }, 'COMMAND', 'daily', [{ message: { author: user } }]);
-        }
-
-        const operationString = (finishTime) => `Operation took: ${new TimeLeftObject(new Date(startTime), new Date(finishTime)).toString()}`;
-        message.channel.send(`Gave achievements!\n${operationString(Date.now())}`);
-
-        //for (var pUser of Users) {
-        //    let achievements = pUser.achievementConfig.achievements.filter((a, _, arr) => {
-        //        const achievementCount = arr.filter(ac => ac._id == a._id).length;
-        //        if (achievementCount > 1) return null;
-        //    });
-
-        //    pUser.achievementConfig.achievements = achievements;
-        //    await PinguUser.UpdatePUser(client, { achievementConfig: pUser.achievementConfig }, pUser, module.exports.name,
-        //        `Updated ${pUser.tag}'s achievements to not include duplicates.`,
-        //        `Failed to update ${pUser.tag}'s achievements to not include duplicates.`
-        //    );
-        //}
-
-        //return message.channel.send(`Updated duplicates.\n${operationString(Date.now())}`);
     });
