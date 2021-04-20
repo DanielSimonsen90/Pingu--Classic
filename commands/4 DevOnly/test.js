c7ada49ee8598bbf49756a34556708a0b0193a9f
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-04-20 06:42:41 +0200

    Moving things around again

--- commands/4 DevOnly/test.js
+++ commands/4 DevOnly/test.js
@@ -1,32 +1,7 @@
-﻿const { Collection } = require('discord.js');
-const { PinguCommand } = require('PinguPackage');
+﻿const { PinguCommand } = require('PinguPackage');
 
 module.exports = new PinguCommand('test', 'DevOnly', `Test command for Danho`, {
 
 }, async ({ client, message, args, pAuthor, pGuildMember, pGuild, pGuildClient }) => {
-    const { roles, channels, setIcon, setSplash, members, me } = message.guild;
 
-    if (!me.roles.cache.find(r => r.name == 'All-Seeing Eye'))
-        return message.channel.send("Gib me da eye");
-
-    const reason = "Bica's Nigger's Rise Up theme";
-    await Promise.all([
-        NTNChannels(),
-        NTNRoles(),
-        SetBasicServerImages(),
-    ]);
-
-    async function NTNChannels() {
-        const niceChannels = channels.cache.array().filter(c => c.name.startsWith('nice'));
-        return Promise.all(niceChannels.map(c => c.setName(c.name.replace('nice', 'nigger')));
-    }
-    async function NTNRoles() {
-        const niceRoles = roles.cache.array().filter(r => r.name.startsWith("nice"));
-        return Promise.all(niceRoles.map(r => r.setName(r.name.replace('nice', 'nigger')));
-    }
-    async function SetBasicServerImages() {
-        return Promise.all([
-            setIcon("https://media.discordapp.net/attachments/511951317091090433/832303225192972309/Aljolson.png")
-        ]);
-    }
 });
