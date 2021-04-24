9ebce2d9f5cd4d6d09423d2289366b3b959fd95a
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-04-24 02:42:21 +0200

    getBadges

--- commands/3 Supporting/invite.js
+++ commands/3 Supporting/invite.js
@@ -1,8 +1,7 @@
 const { PinguCommand, PinguLibrary } = require('PinguPackage');
 
 module.exports = new PinguCommand('invite', 'Supporting', 'Sends link to invite bot to your server', null, async ({ message }) => {
-    let pinguPermissions = 0;
-    PinguLibrary.Permissions().given.forEach(perm => pinguPermissions += perm.bit);
+    let pinguPermissions = PinguLibrary.Permissions().given.reduce((pinguPermissions, perm) => pinguPermissions += perm.bit, 0);
 
     return message.channel.send(`https://discord.com/api/oauth2/authorize?client_id=${message.client.user.id}&permissions=${pinguPermissions}&scope=applications.commands%20bot`);
 });
