9bea32ab0b068561807c7f9e595281151b6e4871
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-04-24 02:41:31 +0200

    semicolon is cool

--- commands/4 DevOnly/Info/version.js
+++ commands/4 DevOnly/Info/version.js
@@ -1,5 +1,5 @@
 const { PinguCommand } = require('PinguPackage');
 
 module.exports = new PinguCommand('version', 'DevOnly', `Sends the version I'm running`, null, async ({ client, message }) => {
-    return message.channel.send(`Currently running version **${client.config.version}**`)
+    return message.channel.send(`Currently running version **${client.config.version}**`);
 });
