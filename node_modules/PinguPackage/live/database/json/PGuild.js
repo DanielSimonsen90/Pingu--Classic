f00acbae88de2a8444db2b56cec6ec0b865f7e45
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-06-14 07:13:55 +0200

    I don't understand how it fellback to this

--- node_modules/PinguPackage/live/database/json/PGuild.js
+++ node_modules/PinguPackage/live/database/json/PGuild.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PGuild = void 0;
-const Pitem_1 = require("./Pitem");
-class PGuild extends Pitem_1.default {
+const PItem_1 = require("./PItem");
+class PGuild extends PItem_1.default {
     constructor(guild) {
         super(guild);
     }
