a879ce237e3f607183accae25752cf6a15765780
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-08-10 17:18:38 +0200

    fix config

--- src/helpers/Config.ts
+++ src/helpers/Config.ts
@@ -1,19 +1,12 @@
-interface IEmailer {
-    id: string,
-    secret: string,
-    refreshToken: string
-}
-
 export default interface IConfigRequirements {
     Prefix: string,
-    BetaPrefix?: string,
     token: string,
+    version?: number,
+    BetaPrefix?: string,
     mongoPass?: string,
     api_key?: string,
     youtube_api?: string,
     google_custom_search?: string,
-    emailer: IEmailer
-    version?: number,
     testingMode?: boolean,
     updateStats?: boolean
 }
